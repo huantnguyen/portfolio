30d5f11d558cf7582317cda2dedc584cdad70174
huantnguyen <huanguyen@g.ucla.edu>
2020-02-05 02:54:46 +0000

    Added all the live links, do some make over for the whole page. Just missing different media screens.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function handleThumbnailClicks() {
   $('.thumbnail').on('click', function(event) {
-     const imgSrc = $(event.currentTarget).find('img').attr('src');  
-     const imgAlt = $(event.currentTarget).find('img').attr('alt');
+    const imgSrc = $(event.currentTarget).find('img').attr('src');  
+    const imgAlt = $(event.currentTarget).find('img').attr('alt');
     
     $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
   });
