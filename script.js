cf0dc9cf4b94e51cbc99363cd661e94d887a3b1b
huantnguyen <huanguyen@g.ucla.edu>
2020-03-02 05:45:04 +0000

    Finished the website. Add a project. Change a lot of CSS, including different media screens. Fixed Javascript error.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function handleThumbnailClicks() {
   $('.thumbnail').on('click', function(event) {
     const imgSrc = $(event.currentTarget).find('img').attr('src');  
     const imgAlt = $(event.currentTarget).find('img').attr('alt');
-    
-    $('.hero').attr('src', imgSrc).attr('alt', imgAlt);
+    console.log(imgSrc);
+    $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
   });
 }
 
