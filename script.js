9a6931470e6d185d3a9d58159ff9cf553439a90d
huantnguyen <huanguyen@g.ucla.edu>
2020-03-02 03:05:20 +0000

    Minor stuff

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function handleThumbnailClicks() {
     const imgSrc = $(event.currentTarget).find('img').attr('src');  
     const imgAlt = $(event.currentTarget).find('img').attr('alt');
     
-    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    $('.hero').attr('src', imgSrc).attr('alt', imgAlt);
   });
 }
 
